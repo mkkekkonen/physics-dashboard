cab12566aa8a979579406552ec5d66756024af14
mkkekkonen2 <mkkekkonen@protonmail.com>
2022-08-11 00:06:27 +0300

    Coarse border collision handling

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -15,6 +15,12 @@ const CIRCLE_RADIANS = 2 * Math.PI;
 const OMEGA_RANGE_PERCENTAGE = 0.06;
 const OMEGA_RANGE_ABS = CIRCLE_RADIANS * OMEGA_RANGE_PERCENTAGE;
 
+const MIN_POSITION_X = 50;
+let MAX_POSITION_X;
+
+const MIN_POSITION_Y = 50;
+let MAX_POSITION_Y;
+
 const getPolygonInitialState = (id, position) => {
   const polygon = generateRandomPolygon();
 
@@ -33,8 +39,11 @@ const getPolygonInitialState = (id, position) => {
 export function* generatePolygons(action) {
   const { width, height } = action.payload;
 
-  const poly1XRange = [50, width / 2];
-  const poly2XRange = [width / 2, width - 50];
+  MAX_POSITION_X = width - 50;
+  MAX_POSITION_Y = height - 50;
+
+  const poly1XRange = [MIN_POSITION_X, width / 2];
+  const poly2XRange = [width / 2, MAX_POSITION_X];
 
   const poly1Position = Vector2.generateRandomFromCoords(
     poly1XRange[0],
@@ -74,6 +83,24 @@ export function* updatePolygons(action) {
     const rotationDelta = polygon.get('angularVelocity') * deltaSeconds;
     const newRotation = polygon.get('rotation') + rotationDelta;
     polygons = polygons.setIn([i, 'rotation'], newRotation);
+
+    if (newPosition.x < MIN_POSITION_X
+      || (MAX_POSITION_X && newPosition.x > MAX_POSITION_X)
+    ) {
+      const multiplier = new Vector2({ x: -1, y: 1 }); // invert X component
+      const newVelocity = polygon.get('velocity').multiplyVector(multiplier);
+      polygons = polygons.setIn([i, 'velocity'], newVelocity);
+      console.log(newVelocity);
+    }
+
+    if (newPosition.y < MIN_POSITION_Y
+      || (MAX_POSITION_Y && newPosition.y > MAX_POSITION_Y)
+    ) {
+      const multiplier = new Vector2({ x: 1, y: -1 }); // invert Y component
+      const newVelocity = polygon.get('velocity').multiplyVector(multiplier);
+      polygons = polygons.setIn([i, 'velocity'], newVelocity);
+      console.log(newVelocity);
+    }
   }
 
   yield put(savePolygons(polygons));
